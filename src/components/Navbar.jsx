3b3673d6786044802609aeb6bd7588f5b6a50a64
Mohammed-code-9723 <mohameddhimni311@gmail.com>
2024-07-09 16:07:00 +0100

    add login signup forms

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,33 @@ import { MdContactPhone } from "react-icons/md";
 import { MdPersonAdd } from "react-icons/md";
 
 
+import { MdAutoAwesome } from "react-icons/md";
+import { FiLogIn } from "react-icons/fi";
+
+import Modal from '@mui/joy/Modal';
+import ModalClose from '@mui/joy/ModalClose';
+import Typography from '@mui/joy/Typography';
+import Sheet from '@mui/joy/Sheet';
+import { Form,Input, SelectPicker } from 'rsuite';
+
+const selectData = ['Eugenia', 'Bryan', 'Linda', 'Nancy', 'Lloyd', 'Alice'].map(item => ({
+    label: item,
+    value: item
+}));
+
+const Textarea = React.forwardRef((props, ref) => <Input {...props} as="textarea" ref={ref} />);
 export default function Navbar() {
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+
+    const [formValue, setFormValue] = React.useState({
+        name: '',
+        email: '',
+        password: '',
+        textarea: ''
+    });
+
     return (
         <div className='Nav_container'>
             <h1 className='logo'><SiHomeassistant style={{color:'white'}}/>&nbsp;&nbsp;&nbsp;<span style={{color:'white'}}>AssetLink</span></h1>
@@ -36,16 +62,88 @@ export default function Navbar() {
                     </NavLink>
                 </ul>
                 <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', width: '25%' }}>
-                    <Button className="signInBtn" sx={{ borderRadius: '20px',background:'#088fa6' }}>
+                    <Button onClick={handleOpen} className="signInBtn" sx={{ borderRadius: '20px',background:'#088fa6' }}>
                         <MdPersonAdd/>&nbsp; &nbsp; &nbsp;  Sign In
                     </Button>
                 </Box>
             </nav>
-                    {/* <Avatar
+            <div >
+                //! Modal:
+                <Modal
+                    aria-labelledby="modal-title"
+                    aria-describedby="modal-desc"
+                    open={open}
+                    onClose={() => setOpen(false)}
+                    sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                >
+                    <Sheet
                     variant="outlined"
-                    size="sm"
-                    src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
-                    /> */}
+                    sx={{
+                        width:'70%',
+                        borderRadius: 'md',
+                        boxShadow: 'lg',
+                        display:'flex',
+                        height:'90vh',
+                    }}
+                    >
+                        <ModalClose variant="plain" sx={{ m: 1 }} />
+                        <div style={{width:'40%'}}>
+                            <img src="/wallpaper_1.jpg" alt="logImg" style={{
+                                width:'100%',
+                                position:'relative ',
+                                borderTopLeftRadius:'7px',
+                                borderBottomLeftRadius:'7px',
+                                height:'90vh'
+                            }}/>
+                        </div>
+                        <div style={{
+                            width:'60%',
+                            padding:'50px 20px '
+                        }}>
+                            <center>
+                                <h1 style={{marginTop:'-50px'}}>Sign up </h1>
+                            </center>
+                            <Form fluid onChange={setFormValue} formValue={formValue}>
+                                <Form.Group controlId="name-9">
+                                <Form.ControlLabel>Username</Form.ControlLabel>
+                                <Form.Control name="name" />
+                                <Form.HelpText>Required</Form.HelpText>
+                                </Form.Group>
+                                <Form.Group controlId="email-9">
+                                <Form.ControlLabel>Email</Form.ControlLabel>
+                                <Form.Control name="email" type="email" />
+                                <Form.HelpText>Required</Form.HelpText>
+                                </Form.Group>
+                                <Form.Group controlId="password-9">
+                                <Form.ControlLabel>Password</Form.ControlLabel>
+                                <Form.Control name="password" type="password" autoComplete="off" />
+                                </Form.Group>
+                                <Form.Group controlId="textarea-9">
+                                <Form.ControlLabel>Textarea</Form.ControlLabel>
+                                <Form.Control rows={5} name="textarea" accepter={Textarea} />
+                                </Form.Group>
+                                <Form.Group controlId="select-10">
+                                <Form.ControlLabel>SelectPicker</Form.ControlLabel>
+                                <SelectPicker data={selectData} style={{ width: '100%',zIndex:100,marginBottom:'10px' }} placement="rightStart" />
+                                </Form.Group>
+                            </Form>
+                            <div style={{
+                                width:'100%',
+                                display:'flex',
+                                justifyContent:'space-evenly',
+                                // marginTop:'100px'
+                            }}>
+                            <Button onClick={() => setOpen(false)} appearance="primary">
+                                <FiLogIn />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Login
+                            </Button>
+                            <Button onClick={() => setOpen(false)} appearance="subtle">
+                                Cancel
+                            </Button>
+                            </div>
+                        </div>
+                    </Sheet>
+                </Modal>
+            </div>
         </div>
     )
 }
--- src/layouts/LandingPage.jsx
+++ src/layouts/LandingPage.jsx
@@ -3,44 +3,131 @@ import { Button } from '@mui/joy';
 import { MdAutoAwesome } from "react-icons/md";
 import { FiLogIn } from "react-icons/fi";
 
+import Modal from '@mui/joy/Modal';
+import ModalClose from '@mui/joy/ModalClose';
+import Typography from '@mui/joy/Typography';
+import Sheet from '@mui/joy/Sheet';
+import { Form } from 'rsuite';
+
 export default function LandingPage() {
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    
     return (
-        <div style={{
-            background:'linear-gradient(124deg, rgba(7,28,75,1) 0%, rgba(9,100,60,1) 100%)',
-            width:'110%',
-            margin:'-20px',
-            padding:'10px',
-            minHeight:'90dvh',
-            display:'flex',
-            justifyContent:'space-around',
-            alignItems:'center',
-            }}>
+        <>
             <div style={{
-                width:'40%',
-                color:'white',
-                // background:'red',
-                fontWeight:'900',
-                fontSize:'4rem',
-                textAlign:'center'
-                
-            }}>
-                Gestion immobilière <br />
-                sans effort avec <br />
-                AssetLink <br />
-                à l'aide d'AI <MdAutoAwesome size={50}/><br />
-                <Button 
-                // className="signInBtn"
-                sx={{
-                    width:'50%',
-                    borderRadius: '20px',
-                    background:'#088fa6' 
+                background:'linear-gradient(124deg, rgba(7,28,75,1) 0%, rgba(9,100,60,1) 100%)',
+                width:'110%',
+                margin:'-20px',
+                padding:'10px',
+                minHeight:'90dvh',
+                display:'flex',
+                justifyContent:'space-around',
+                alignItems:'center',
                 }}>
-                    <FiLogIn />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Log in
-                </Button>
+                <div style={{
+                    width:'40%',
+                    color:'white',
+                    // background:'red',
+                    fontWeight:'900',
+                    fontSize:'4rem',
+                    textAlign:'center'
+                    
+                }}>
+                    Gestion immobilière <br />
+                    sans effort avec <br />
+                    AssetLink <br />
+                    à l'aide d'AI <MdAutoAwesome size={50}/><br />
+                    <Button 
+                    // className="signInBtn"
+                    onClick={handleOpen}
+                    sx={{
+                        width:'50%',
+                        borderRadius: '20px',
+                        background:'#088fa6' 
+                    }}>
+                        <FiLogIn />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Log in
+                    </Button>
+                </div>
+                <div style={{width:'60%'}}>
+                    <img src="/realEstate.png" alt="realEstate" style={{
+                        width:'100%',
+                        position:'relative ',
+                    }}/>
+                </div>
             </div>
-            <div style={{width:'70%'}}>
-                <img src="/realEstate.png" alt="realEstate" />
+            <div >
+                //! Modal:
+                <Modal
+                    aria-labelledby="modal-title"
+                    aria-describedby="modal-desc"
+                    open={open}
+                    onClose={() => setOpen(false)}
+                    sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                >
+                    <Sheet
+                    variant="outlined"
+                    sx={{
+                        width:'70%',
+                        borderRadius: 'md',
+                        // p: 3,
+                        boxShadow: 'lg',
+                        display:'flex',
+                        height:'70vh'
+                        // justifyContent:'space'
+                    }}
+                    >
+                        <ModalClose variant="plain" sx={{ m: 1 }} />
+                        <div style={{width:'40%'}}>
+                            <img src="/wallpaper_1.jpg" alt="logImg" style={{
+                                width:'100%',
+                                position:'relative ',
+                                borderTopLeftRadius:'7px',
+                                borderBottomLeftRadius:'7px',
+                                height:'70vh'
+                            }}/>
+                        </div>
+                        <div style={{
+                            width:'60%',
+                            padding:'100px 20px '
+                            // display:'flex', 
+                            // flexDirection:'column',
+                            // justifyContent:'center',
+                            // alignItems:'center'
+                        }}>
+                            <center>
+                                <h1 style={{marginTop:'-50px'}}>Log in </h1>
+                            </center>
+                            <Form fluid>
+                                <Form.Group controlId="email-1">
+                                <Form.ControlLabel>Email</Form.ControlLabel>
+                                <Form.Control name="email" type="email" />
+                                <Form.HelpText>Required</Form.HelpText>
+                                </Form.Group>
+                                <Form.Group controlId="password-1">
+                                <Form.ControlLabel>Password</Form.ControlLabel>
+                                <Form.Control name="password" type="password" autoComplete="off" />
+                                <Form.HelpText>Required</Form.HelpText>
+                                </Form.Group>
+                            </Form>
+                            <div style={{
+                                width:'100%',
+                                display:'flex',
+                                justifyContent:'space-evenly',
+                                marginTop:'100px'
+                            }}>
+                            <Button onClick={() => setOpen(false)} appearance="primary">
+                                <FiLogIn />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Login
+                            </Button>
+                            <Button onClick={() => setOpen(false)} appearance="subtle">
+                                Cancel
+                            </Button>
+                            </div>
+                        </div>
+                    </Sheet>
+                </Modal>
             </div>
-        </div>
+        </>
     )
 }
